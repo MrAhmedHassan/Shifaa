8719c9c90372afd59c7bc1a6e3f059c55f762d95
mohamed <mohammedgammal787@gmail.com>
2020-01-29 20:13:10 +0200

    modify reservation table

--- database/migrations/2020_01_27_115651_create_reservations_table.php
+++ database/migrations/2020_01_27_115651_create_reservations_table.php
@@ -15,12 +15,11 @@ class CreateReservationsTable extends Migration
     {
         Schema::create('reservations', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('reveal_id')->nullable()->on('users')->onUpdate('cascade')->onDelete('set null');
-            $table->foreign('reveal_id')->references('id')->on('reveals');
+          /*  $table->unsignedBigInteger('reveal_id')->nullable()->on('users')->onUpdate('cascade')->onDelete('set null');*/
+            /*$table->foreign('reveal_id')->references('id')->on('reveals');*/
             $table->unsignedBigInteger('patient_id')->nullable()->on('users')->onUpdate('cascade')->onDelete('set null');
             $table->foreign('patient_id')->references('id')->on('users');
-            $table->unsignedBigInteger('reveal_id')->nullable()->on('users')->onUpdate('cascade')->onDelete('set null');
-            $table->foreign('reveal_id')->references('id')->on('reveals');
+            
             $table->unsignedBigInteger('doctor_id')->nullable()->on('users')->onUpdate('cascade')->onDelete('set null');
             $table->foreign('doctor_id')->references('id')->on('users');
             $table->timestamp('deleted_at')->nullable();
