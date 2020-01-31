56db02a278b956f34fe44d2f2a84b38a1f884163
mohamed <mohammedgammal787@gmail.com>
2020-02-01 00:57:12 +0200

    ceateing personal test

--- resources/views/human_body/show.blade.php
+++ resources/views/human_body/show.blade.php
@@ -7,40 +7,35 @@
         <div class="row">
             <div class="col-md-8">
                 <div class="content">
+               
+               
+                
                     <div class="container">
                         <div class="row pt-5 d-flex justify-content-center mb-5">
-                            <img src="{{ asset('/imgs/Brain.png') }}" alt="Brain" class="brainImag">
+                           
+                            
+                            <img src= "../images/{{$humanbody->image}}" class="brainImag">
                         </div>
                         <div class="row">
                             <div class="col-md-12">
-                                <h2 class="text-center text-danger">الجهاز العصبي</h2>
+                                <h2 class="text-center text-danger">{{$humanbody->title}}</h2>
                             </div>
                         </div>
+                       
                         <div class="row">
                             <div class="col-md-8 m-auto text-break">
                                 <p class="text-right mb-5">
-                                    هو مجموعة معقدة من الأعصاب والخلايا المتخصصة المعروفة باسم الخلايا العصبية التي
-                                    تنقل إشارات بين أجزاء مختلفة من الجسم. <br>
-                                    يتكون الجهاز العصبي من جزئين: <br>
-                                    <ol class="text-right mb-5">
-                                        <li> <span class="text-danger">الجهاز العصبي المركزي</span>: يتكون الجهاز
-                                            العصبي المركزي من الدماغ والحبل الشوكي والأعصاب.</li>
-                                        <li>
-                                            <span class="text-danger">الجهاز العصبي المحيطي</span>: يتكون الجهاز
-                                            العصبي المحيطي من الخلايا العصبية الحسية، العقد (مجموعات من الخلايا
-                                            العصبية) والأعصاب التي تتصل بعضها البعض والجهاز العصبي المركزي.
-                                        </li>
-                                    </ol>
-                                    <p class="text-right">
-                                        يقوم الجهاز العصبي بتنظيم بعض عمليات الجسم مثل ضغط الدم ومعدل التنفس، والتي
-                                        تعمل دون جهد واعي، ويتكون النظام الجسدي من الأعصاب التي تربط الدماغ والحبل
-                                        الشوكي مع العضلات والمستقبلات الحسية في الجلد.
-                                    </p>
+                                   
+                                {{$humanbody->description}}
+                                  
                                 </p>
                             </div>
                         </div>
 
                     </div>
+                   
+
+
                 </div>
             </div>
             <div class="col-md-4">
